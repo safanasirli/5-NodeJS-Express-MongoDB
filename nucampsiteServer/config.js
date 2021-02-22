9bc7b33a476a5e5d17705c31ff65c9097db001c4
safanasirli <76178637+safanasirli@users.noreply.github.com>
2021-02-22 11:22:54 -0500

    Add files via upload

---
+++ nucampsiteServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '288239792724385',
+        clientSecret: 'ef97ae2f50ecf11c0fb8e1c762c0663e'
+    }
+}
